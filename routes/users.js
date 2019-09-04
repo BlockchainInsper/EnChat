5b2833f8e9245e61df5d2e1aee0ba1caa3af9503
Bruno Arthur <brunoartc@yahoo.com>
2019-09-03 22:12:12 -0300

    add chat endpoint & format code

--- routes/users.js
+++ routes/users.js
@@ -3,56 +3,60 @@ var router = express.Router();
 var users = require('../resources/users_db')
 
 /* GET users listing. */
-router.post('/create', function (req, res, next) {
-  let {username, publicKey} = req.body
-  publicKey = publicKey.replace(/\\n/g,"\n") //TODO: find new way without RegEx
-  usernameValue = req.body.username;
-  
-  users.saveUserAndPubKey(username, publicKey).then((resp) => {
+router.post('/create', function(req, res, next) {
+    let { username, publicKey } = req.body
+    publicKey = publicKey.replace(/\\n/g, "\n") //TODO: find new way without RegEx
+    usernameValue = req.body.username;
 
-    if (resp.status == 'success') {
-      console.log(resp.data);
-      console.log(resp.data.randomMsg);
-      
-      
-      res.render('authentication', {randomVar: resp.data.randomMsg, usernameValue})
-    } else {
-      res.render('fail')
-    }
+    users.saveUserAndPubKey(username, publicKey).then((resp) => {
 
-  
-  }).catch((err) => console.log(err))
+        if (resp.status == 'success') {
+            console.log(resp.data);
+            console.log(resp.data.randomMsg);
+
+
+            res.render('authentication', { randomVar: resp.data.randomMsg, usernameValue })
+        } else {
+            res.render('fail')
+        }
+
+
+    }).catch((err) => console.log(err))
 });
 
-router.post('/confirm', function (req, res, next) {
-  let {username, signature} = req.body
-  users.confirmKey(username, signature).then((resp) => {
+router.post('/confirm', function(req, res, next) {
+    let { username, signature } = req.body
+    users.confirmKey(username, signature).then((resp) => {
 
-    if (resp.status == 'success') {
-      res.render('chat')
-    } else {
-      res.render('fail')
-    }
-    
+        if (resp.status == 'success') {
+            res.render('chat')
+        } else {
+            res.render('fail')
+        }
 
 
-  }).catch((err) => console.log(err))
+
+    }).catch((err) => console.log(err))
 });
 
-router.post('/generate', function (req, res, next) {
-  let {masterKey} = req.body
-  users.getMasterKey().then((resp) => console.log(resp)).catch((err) => console.log(err))
-  users.genUserKey(masterKey).then((resp) => res.send(resp)).catch((err) => console.log(err))
+router.post('/generate', function(req, res, next) {
+    let { masterKey } = req.body
+    users.getMasterKey().then((resp) => console.log(resp)).catch((err) => console.log(err))
+    users.genUserKey(masterKey).then((resp) => res.send(resp)).catch((err) => console.log(err))
 });
 
 
-router.get('/create', function (req, res, next) {
-  res.render('index')
+router.get('/create', function(req, res, next) {
+    res.render('index')
 });
 
-router.get('/list', function (req, res, next) {
-  let {user} = req.body
-  users.listUsers(user).then((resp) => res.send(resp)).catch((err) => console.log(err))
+router.get('/list', function(req, res, next) {
+    let { user } = req.body
+    users.listUsers(user).then((resp) => res.send(resp)).catch((err) => console.log(err))
+});
+
+router.get('/chat', function(req, res, next) {
+    res.render('chat')
 });
 
 module.exports = router;
