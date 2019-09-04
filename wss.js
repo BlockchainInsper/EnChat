ed308b400d0bea640ac439052e480d7b22fc9c11
Bruno Arthur <brunoartc@yahoo.com>
2019-09-03 22:10:20 -0300

    add dynamic WebSocket hostname & fix messages

--- wss.js
+++ wss.js
@@ -8,9 +8,9 @@ async function init(server) {
 
         const WebSocket = require('ws');
 
-        
 
-        wss = new WebSocket.Server({server});
+
+        wss = new WebSocket.Server({ server });
 
 
         if (wss != undefined) {
@@ -24,38 +24,40 @@ async function init(server) {
                     ws.isAlive = true;
                 });
 
-                ws.on('message', (message) => { 
-                    
+                ws.on('message', (message) => {
+
                     try {
                         let msg = JSON.parse(message)
-                        //TODO:save in database(?) test if user exist and if signature is right
+                            //TODO:save in database(?) test if user exist and if signature is right
                         wss.clients
-                        .forEach(client => {
-                            if (client == ws) {
-                                client.send(`{"data":${message}}`);
+                            .forEach(client => {
+                                if (client == ws) {
+                                    client.send(`{"data":${message},"origin":"you"}`);
 
-                            }    
-                        });
+                                } else {
+                                    client.send(`{"data":${message},"origin":"other"}`);
+                                }
+                            });
                     } catch (error) {
                         console.log(error, message);
                     }
-                    
-                    
+
+
                 })
             });
 
             setInterval(() => {
                 wss.clients.forEach((ws) => {
-                    
+
                     if (!ws.isAlive) return ws.terminate();
-                    
+
                     ws.isAlive = false;
                     ws.ping(null, false, true);
                 });
             }, 10000);
 
 
-            
+
 
 
             resolve("WSS CONFIGURATION DONE")
